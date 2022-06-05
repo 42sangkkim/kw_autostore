92f2aa0248e0be090a4170ac5256f5c469596f41
sangkkim <madlogic365@gmail.com>
2022-06-05 20:35:56 +0900

    copy the code

--- src/autostore/setup.py
+++ src/autostore/setup.py
@@ -1,6 +1,11 @@
-from setuptools import setup
+import os # Operating system library
+from glob import glob # Handles file path names
+from setuptools import setup # Facilitates the building of packages
 
 package_name = 'autostore'
+
+# Path of the current directory
+cur_directory_path = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name=package_name,
@@ -10,16 +15,32 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        # Path to the launch file      
+        (os.path.join('share', package_name,'launch'), glob('launch/*.launch.py')),
+
+        # Path to the world file
+        (os.path.join('share', package_name,'worlds/'), glob('./worlds/*')),
+
+        # Path to the warehouse sdf file
+        (os.path.join('share', package_name,'models/grid/'), glob('./models/grid/*')),
+
+        # Path to the mobile robot sdf file
+        (os.path.join('share', package_name,'models/blackline/'), glob('./models/blackline/*')),
+        
+        # Path to the world file (i.e. warehouse + global environment)
+        (os.path.join('share', package_name,'models/'), glob('./worlds/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='sangkkim',
+    maintainer='Kim Sangkyeong',
     maintainer_email='madlogic365@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+          'spawn_demo = autostore.spawn_demo:main'
         ],
     },
 )
